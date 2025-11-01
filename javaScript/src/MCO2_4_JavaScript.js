1c874e5c1c8f40beaa8d20f197570055111ef648
ASSistant Chef of the 4th Axis Armenia Batallion <160565653+FlyingOwOL@users.noreply.github.com>
2025-11-01 13:48:12 +0800

    Added flow

--- javaScript/src/MCO2_4_JavaScript.js
+++ javaScript/src/MCO2_4_JavaScript.js
@@ -5,5 +5,23 @@ const prompt = promptSync();
 let userChoice = null;
 
 do{
-    userChoice = prompt("Exit?");
+    console.log("Select Language Implementation:\n" +
+                "[1] Load the file\n" + 
+                "[2] Generate reports\n" +
+                "[3] Exit program\n");
+    userChoice = prompt("Enter choice: ");
+    switch(userChoice){
+        case '1':
+            console.log("in-progress...\n\n\n");
+            break;
+        case '2':
+            console.log("in-progress...\n\n\n");
+            break;
+        case '3':
+            console.log("Exiting program...\n");
+            break;
+        default:
+            console.log("Invalid option\n");
+            break;
+    }
 }while(userChoice != 3);
