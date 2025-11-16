f4c298a7b3e4079d2a7fa666375e8c682af91149
ASSistant Chef of the 4th Axis Armenia Batallion <160565653+FlyingOwOL@users.noreply.github.com>
2025-11-16 22:58:51 +0800

    Update summary.js

--- javaScript/src/generateReports/summary.js
+++ javaScript/src/generateReports/summary.js
@@ -1,7 +1,19 @@
+import { generateJSON } from "./assist.js";
+
 export function generateSummary (data){
     console.log("Summary Stats (summary.json:")
     try{
-        
+        const summary = {
+            total_projects : 0, 
+            total_contractors : 0, 
+            total_provinces : 0, 
+            total_regions: 0, 
+            global_avg_delay: 0, 
+            global_total_savings: 0,
+            global_total_budget: 0,
+            date_range: 0
+        };
+        generateJSON(summary);w
     } catch (error){
         console.log("Error generating Summary.JSOn " + error.message);
     }    
